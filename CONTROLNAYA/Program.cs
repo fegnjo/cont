469320aecc54d668695f56651064f945e262f940
aleksey yagofarov <aleksey-yagofarov@yandex.ru>
2023-04-06 18:27:26 +0300

    first files commit

---
+++ CONTROLNAYA/Program.cs
@@ -0,0 +1,18 @@
+﻿string [] array = {"Hello", "top", "buy", "origin", "master", ":-)"}; 
+
+string [] newArray = new string [GetQuantity()];
+
+
+int GetQuantity()
+{
+    int indexNewArray = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            indexNewArray ++;
+        }
+    }
+    return indexNewArray;
+}
+
