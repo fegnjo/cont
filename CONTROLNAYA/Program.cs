f85e085b4cdb3dabb02849a310b1d2c3398fe6dc
aleksey yagofarov <aleksey-yagofarov@yandex.ru>
2023-04-06 18:28:08 +0300

    second files commit

--- CONTROLNAYA/Program.cs
+++ CONTROLNAYA/Program.cs
@@ -1,6 +1,7 @@
 ﻿string [] array = {"Hello", "top", "buy", "origin", "master", ":-)"}; 
 
 string [] newArray = new string [GetQuantity()];
+FindWords();
 
 
 int GetQuantity()
@@ -15,4 +16,18 @@ int GetQuantity()
     }
     return indexNewArray;
 }
+
+void FindWords()
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            newArray[index] = array[i];
+            index ++;
+        }
+    }
+}
+
 
